5b08300a160ec5bf7a6a0680357aa137c1e01b12
Manu <116085483+theperk08@users.noreply.github.com>
2023-07-07 11:14:53 +0200

    Update Dash_Layouts.py
    input interactive graph callbacks

--- Dash_Layouts.py
+++ Dash_Layouts.py
@@ -14,7 +14,7 @@ import plotly.express as px
 url = 'https://raw.githubusercontent.com/chriszapp/datasets/main/books.csv'
 df = pd.read_csv(url, nrows = 3000)
 
-df2 = df.groupby(by = 'authors').mean().reset_index()
+df2 = df.groupby(by = ['authors', 'title']).agg({'language_code' : 'first', '  num_pages' : 'mean'}).reset_index()
 fig = px.scatter(df2, x="authors", y="  num_pages")
 fig.update_layout(
     title=dict(text="Nombre moyen de pages par auteur", font=dict(size=30), automargin=True, yref='paper'),
@@ -30,12 +30,11 @@ fig.update_layout(
 app = Dash(__name__)
 server = app.server
 
-app.layout = html.Div([html.Div(
+app.layout = html.Div([
     html.H4(children='Books'),
     dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    ),id ='output-container'),
+                figure=fig
+    ,id ='graph'),
                            
     dcc.Markdown('''                   
                
@@ -45,7 +44,7 @@ app.layout = html.Div([html.Div(
 
 '''), 
     # choix auteur
-    dcc.Dropdown(list(df['authors'].unique()),
+    dcc.Dropdown(list(df['authors'].unique()), 'J.K. Rowling',
                  id = 'drop-authors'),
    
     dcc.Markdown('''
@@ -63,14 +62,14 @@ Choix de la langue :
                  
                  
 @callback(
-    Output('dd-output-container', 'children'),
+    Output('graph', 'figure'),
     [Input('drop-authors', 'value'),
     Input('radio-lang', 'value')]
 )
 
 def update_graph(auteur, langue):
     df3 = df2[(df2['authors'] == auteur) & (df2['language_code'] == langue)]
-    fig = px.scatter(df3, x = "title", y = "  num_pages")
+    fig = px.scatter(df3, x="title", y="  num_pages")
     return fig
 
 
