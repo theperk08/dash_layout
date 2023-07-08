e0d2173c337e7cd4d7b2f267e4e2fb069eb43b89
Manu <116085483+theperk08@users.noreply.github.com>
2023-07-08 09:04:34 +0200

    Update Dash_Layouts.py
    dbc components (row, col)

--- Dash_Layouts.py
+++ Dash_Layouts.py
@@ -7,6 +7,8 @@ Created on Fri Jul  7 09:38:29 2023
 
 from dash import Dash, html, dcc, Input, Output, callback
 
+import dash_bootstrap_components as dbc
+
 import pandas as pd
 import plotly.express as px
 
@@ -26,39 +28,55 @@ fig.update_layout(
 
 
 
-
-app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-app.layout = html.Div([
+app.layout = dbc.Container([
     html.H4(children='Books'),
     dcc.Graph(
                 figure=fig
     ,id ='graph'),
+    
+    dbc.Row([
+        dbc.Col(
                            
-    dcc.Markdown('''                   
+            dcc.Markdown('''                   
                
                  
                
                  Choix de l'auteur :
 
-'''), 
-    # choix auteur
-    dcc.Dropdown(list(df['authors'].unique()), 'J.K. Rowling',
-                 id = 'drop-authors'),
-   
-    dcc.Markdown('''
-                   
+                     '''),
+                     width=6),
+        dbc.Col(
+            dcc.Markdown('''                   
                  
               
                  
-Choix de la langue :
+              Choix de la langue :
 
-'''), 
-    # choix langue
-    dcc.RadioItems(list(df['language_code'].unique()), 'eng',
-                   id = 'radio-lang')
-])
+                  '''),
+                  width = 6)
+                ]),
+                
+    dbc.Row([
+        dbc.Col(
+            # choix auteur
+    
+            dcc.Dropdown(options = [{'label':nom, 'value': nom} for nom in list(df['authors'].unique())],
+                                value =list(df['authors'].unique())[0],
+                                    id ="drop-authors"                                    
+                                    ),
+            width = 6),
+        dbc.Col(
+    
+        # choix langue
+        dcc.RadioItems(options = [{'label':nom, 'value': nom} for nom in list(df['language_code'].unique())],
+                   value =list(df['language_code'].unique())[0],                                     
+                   id = 'radio-lang'),
+            width = 6)
+        ])
+    ])
                  
                  
 @callback(
@@ -68,6 +86,7 @@ Choix de la langue :
 )
 
 def update_graph(auteur, langue):
+   
     df3 = df2[(df2['authors'] == auteur) & (df2['language_code'] == langue)]
     fig = px.scatter(df3, x="title", y="  num_pages")
     return fig
